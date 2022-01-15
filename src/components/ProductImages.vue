f1f4ecf63d33e9d768e7f8adde8dd3d83d47a92f
Rama H <kizotti@hotmail.fr>
2022-01-15 23:51:51 +0100

    changes

--- src/components/ProductImages.vue
+++ src/components/ProductImages.vue
@@ -1,41 +1,40 @@
 <template>
-<section class="preview">
-        <div class="preview-main">
-          <div ref="productImagesSlider" class="container-product-images">
-            <img src="../assets/image-product-1.jpg" alt="Image product" class="product-images" />
-            <img src="../assets/image-product-2.jpg" alt="Image product" class="product-images" />
-            <img src="../assets/image-product-3.jpg" alt="Image product" class="product-images" />
-            <img src="../assets/image-product-4.jpg" alt="Image product" class="product-images" />
-
-          </div>
-          <div class="preview-mobile-selection">
-            <div @click="right" class="rounded-button">
-              <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd" />
-              </svg>
-            </div>
-            <div @click="left" class="rounded-button">
-              <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
-                <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd" />
-              </svg>
-            </div>
-          </div>
+  <section class="preview">
+    <div class="preview-main">
+      <div ref="productImagesSlider" class="container-product-images">
+        <img src="../assets/image-product-1.jpg" alt="Image product" class="product-images" />
+        <img src="../assets/image-product-2.jpg" alt="Image product" class="product-images" />
+        <img src="../assets/image-product-3.jpg" alt="Image product" class="product-images" />
+        <img src="../assets/image-product-4.jpg" alt="Image product" class="product-images" />
+      </div>
+      <div class="preview-mobile-selection">
+        <div @click="right" class="rounded-button">
+          <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd" />
+          </svg>
         </div>
-        <div class="preview-selection">
-          <div class="preview-image-1 active">
-            <img src="../assets/image-product-1-thumbnail.jpg" alt="Image product 1">
-          </div>
-          <div class="preview-image-2">
-            <img src="../assets/image-product-2-thumbnail.jpg" alt="Image product 2">
-          </div>
-          <div class="preview-image-3">
-            <img src="../assets/image-product-3-thumbnail.jpg" alt="Image product 3">
-          </div>
-          <div class="preview-image-4">
-            <img src="../assets/image-product-4-thumbnail.jpg" alt="Image product 4">
-          </div>
+        <div @click="left" class="rounded-button">
+          <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd" />
+          </svg>
         </div>
-      </section>
+      </div>
+    </div>
+    <div class="preview-selection">
+      <div class="preview-image-1 active">
+        <img src="../assets/image-product-1-thumbnail.jpg" alt="Image product 1" />
+      </div>
+      <div class="preview-image-2">
+        <img src="../assets/image-product-2-thumbnail.jpg" alt="Image product 2" />
+      </div>
+      <div class="preview-image-3">
+        <img src="../assets/image-product-3-thumbnail.jpg" alt="Image product 3" />
+      </div>
+      <div class="preview-image-4">
+        <img src="../assets/image-product-4-thumbnail.jpg" alt="Image product 4" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -52,43 +51,48 @@ export default {
       show: false,
     };
   },
-  mounted: () => {
-
-  let draggableSlider = Draggable.create(".container-product-images", {
-    type:"x",
-    // bounds: document.getElementById("container"),
-    zIndexBoost: false,
-    inertia: true,
-    throwProps:true,
-    onClick:function() {
-      console.log("clicked");
-    },
-    onDragEnd:function() {
-      console.log("drag ended");
-    }
-  });
-
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        
+        let slideWidth = document.querySelector('.product-images').offsetWidth
+        console.log('slideWidth :>> ', slideWidth);
+        let draggableSlider = Draggable.create(".container-product-images", {
+          type: "x",
+          zIndexBoost: false,
+          inertia: true,
+          snap: {
+            x: gsap.utils.snap(slideWidth),
+          },
+          throwProps: true,
+          onClick: function () {
+            console.log("clicked");
+          },
+          onDragEnd: function () {
+            console.log("drag ended");
+          },
+        });
+      }
+    };
   },
   methods: {
     left() {
-      console.log('click')
-      gsap.to(this.$refs.productImagesSlider, {duration: .6, x: '-=100%', ease:'power4.inOut'})
+      console.log("click");
+      gsap.to(this.$refs.productImagesSlider, { duration: 0.6, x: "-=100%", ease: "power4.inOut" });
     },
     right() {
-      gsap.to(this.$refs.productImagesSlider, {duration: .6, x: '+=100%', ease:'power4.inOut'})
-
-    }
-   
+      gsap.to(this.$refs.productImagesSlider, { duration: 0.6, x: "+=100%", ease: "power4.inOut" });
+    },
   },
 };
 </script>
 
 <style scoped>
-
 .preview-main {
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
 }
 
 .preview-selection {
@@ -97,11 +101,11 @@ export default {
 
 .preview-main .container-product-images {
   display: flex;
-  /*overflow: hidden;*/
   width: 100%;
 }
 
 .preview-main .product-images {
+  display: block;
   max-width: 100%;
   height: 100%;
 }
@@ -135,7 +139,6 @@ export default {
 }
 
 @media only screen and (min-width: 750px) {
-
   .preview {
     max-width: 460px;
   }
@@ -147,7 +150,7 @@ export default {
   }
 
   .preview .preview-selection > div {
-    border-radius: .75rem;
+    border-radius: 0.75rem;
     cursor: pointer;
     display: flex;
     overflow: hidden;
@@ -158,7 +161,7 @@ export default {
   }
 
   .preview .preview-selection > div.active img {
-    opacity: .5;
+    opacity: 0.5;
   }
 
   .preview .preview-selection > div:not(:first-child) {
@@ -177,7 +180,5 @@ export default {
   .preview-main #preview {
     border-radius: 0.75rem;
   }
-
-
 }
 </style>
